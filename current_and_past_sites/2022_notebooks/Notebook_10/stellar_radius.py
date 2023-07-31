4c9f65f40aa4b18e6024ccbb9cd708c034767b81
daxfeliz <dfeliz@amnh.org>
2023-07-31 16:54:05 -0400

    updated astropy, astroquery, latex notebooks

--- current_and_past_sites/2022_notebooks/Notebook_10/stellar_radius.py
+++ current_and_past_sites/2022_notebooks/Notebook_10/stellar_radius.py
@@ -2,8 +2,13 @@
 import numpy as np
 import sys
 
-def stellar_radius(mass):
-    starR=mass**0.8
+def stellar_r(mass):
+    if mass<1: #smaller stars than sun
+        starR=mass**0.8
+    if mass>1:#larger stars than sun
+        starR=mass**0.57
+    if mass==1: #stars with equal mass of sun
+        starR=1
     return starR
 
 if __name__ == "__main__":
--- current_and_past_sites/2023_notebooks/Notebook_03/hello.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-msg = "Roll a dice"
-print(msg)
-
-print(np.random.randint(1,6))
