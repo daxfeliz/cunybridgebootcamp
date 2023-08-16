730658d36030023f2d4255f5b138b6a889145cba
daxfeliz <dfeliz@amnh.org>
2023-08-16 09:57:30 -0400

    updated notebooks

--- current_and_past_sites/2023_notebooks/Session_04/modules/adv/sqrt.py
+++ current_and_past_sites/2023_notebooks/Session_04/modules/adv/sqrt.py
@@ -3,7 +3,6 @@
 # NAME
 # DATE
 # CUNY Bridge Program
-from __future__ import print_function, division, absolute_import
 __author__     =['YOUR NAME']
 __copyright__  =["Copyright 2022 YOUR NAME, Name of Project"]
 __email__      =['Email Address']
