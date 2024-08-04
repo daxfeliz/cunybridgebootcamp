ff0a07f5bd24ef55593c9f8d958c1495a393399d
daxfeliz <dfeliz@amnh.org>
2024-08-04 17:59:22 -0400

    Add files via upload

---
+++ current_and_past_sites/2024_notebooks/Session_04/modules/adv/sqrt.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+# NAME
+# DATE
+# CUNY Bridge Program
+__author__     =['YOUR NAME']
+__copyright__  =["Copyright 2022 YOUR NAME, Name of Project"]
+__email__      =['Email Address']
+__maintainer__ =['Your Name']
+__version__     =['Version Number Of Your Code']
+
+__all__=["squareroot"]
+
+import math
+ 
+#----------------------------------------------------------------------
+def squareroot(n):
+    """"""
+    return math.sqrt(n)
